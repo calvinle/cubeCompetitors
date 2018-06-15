8efe11623b08c18dfd23dc74de2834865ad973e3
Calvin <calvin.md.le@gmail.com>
2018-06-14 19:07:45 -0600

    No longer have to put '.cvs' for parameters

--- scratch.py
+++ scratch.py
@@ -2,19 +2,23 @@ import csv
 import sys
 
 if __name__ == "__main__":
-    f = open(sys.argv[1])
-    newF = open(sys.argv[2], "w+")
+    #Declare files from args, read in from input
+    f = open(sys.argv[1] +".csv")
+    newF = open(sys.argv[2] +".csv", "w+")
     reader = csv.reader(f)
     entries = []
     for row in reader:
         entries.append(row)
-        entries.pop(0)
+    entries.pop(0)
 
+    #Create list of competitors
     competitors = []
     for x in entries:
         competitors.append(x[1])
     competitors = sorted(set(competitors))
+    print(competitors)
 
+    #Per competitor, list their events followed by their stage and group
     for x in competitors:
         print(x+",,")
         newF.write(x+",,"+"\n")
@@ -23,6 +27,6 @@ if __name__ == "__main__":
             if x == y[1]:
                 print(y[0] + "," + y[2] + ",")
                 newF.write(y[0] + "," + y[2] + ",\n")
-        print("\n")
+        print(",,")
         newF.write(",,")
         newF.write("\n")
