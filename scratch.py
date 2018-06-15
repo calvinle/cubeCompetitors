1668c6d38a8478b870c3445f418aa9e0fcf37ab7
Calvin <calvin.md.le@gmail.com>
2018-06-14 18:37:46 -0600

    Added commas for csv and inquired format

--- scratch.py
+++ scratch.py
@@ -1,26 +1,28 @@
 import csv
 import sys
-from collections import OrderedDict
 
 if __name__ == "__main__":
     f = open(sys.argv[1])
+    newF = open(sys.argv[2], "w+")
     reader = csv.reader(f)
-    list = []
+    entries = []
     for row in reader:
-        list.append(row)
-    list.pop(0)
+        entries.append(row)
+        entries.pop(0)
 
     competitors = []
-    for x in list:
+    for x in entries:
         competitors.append(x[1])
-    #print(list)
-
     competitors = sorted(set(competitors))
-    #print("Competitors: " + str(competitors) + "\n")
 
     for x in competitors:
-        print(x)
-        for y in list:
+        print(x+",,")
+        newF.write(x+",,"+"\n")
+        newF.write("Event/Round,Stage/Group,\n")
+        for y in entries:
             if x == y[1]:
-                print("EVENT: " + y[0] + " GROUP: " + y[2])
-        print()
+                print(y[0] + "," + y[2] + ",")
+                newF.write(y[0] + "," + y[2] + ",\n")
+        print("\n")
+        newF.write(",,")
+        newF.write("\n")
